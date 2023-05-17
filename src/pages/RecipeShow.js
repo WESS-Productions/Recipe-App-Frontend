f64f81cf5fc3ca3ae1c064fda232c50ff6ca63ac
sam-saenz <samsaenz31@gmail.com>
2023-05-17 14:53:34 -0700

    Attatched recipeshow to mock data

--- src/pages/RecipeShow.js
+++ src/pages/RecipeShow.js
@@ -1,30 +1,35 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 
 
 
 
 const RecipeShow = ({ recipes }) => {
+    const { id } = useParams()
+    console.log(recipes);
+    const currentRecipe = recipes?.find((recipe) => recipe.id === +id)
+
+
     return (
         <div className="recipe-show">
             <div className="picture-section">
-                {/* {picture goes here} */}
-                {/* <img src= alt= /> */}
+                <img src={currentRecipe.image} alt="Picture of food"></img>
             </div>
             <div className="details-section">
-                <h2>Recipe title </h2>
+                <h2>{currentRecipe.title}</h2>
             </div>
             <div className="time-section">
-                <p>Prep time: prep time</p>
-                <p>Cook time: cook time</p>
+                <p>Prep time: {currentRecipe.prep_time}</p>
+                <p>Cook time: {currentRecipe.cook_time}</p>
             </div>
             <div className="ingredients-section">
                 <h3>Ingredients</h3>
-                <ul>ingredients list </ul>
+                <ul>{currentRecipe.ingredients}</ul>
             </div>
             <div className="instructions-section">
                 <h3> Instructions</h3>
-                <p> Intruction section </p>
+                <p>{currentRecipe.instructions}</p>
             </div>
             <div classNmae="show-buttons">
             <button type="button">Click Me!</button>
