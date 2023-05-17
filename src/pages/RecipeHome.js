0e642a4cad92dc69cd43902e64417ae8c9f26d4f
EddieCarrion.github.io <ecar25L@gmail.com>
2023-05-17 16:38:39 -0700

    got our pictures working on the home page

--- src/pages/RecipeHome.js
+++ src/pages/RecipeHome.js
@@ -9,26 +9,27 @@ import {
 
 const items = [
   {
-    src: 'https://picsum.photos/id/123/1200/400',
+    src: 'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
     altText: 'Slide 1',
     caption: 'Slide 1',
     key: 1,
   },
   {
-    src: 'https://picsum.photos/id/456/1200/400',
+    src: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
     altText: 'Slide 2',
     caption: 'Slide 2',
     key: 2,
   },
   {
-    src: 'https://picsum.photos/id/678/1200/400',
+    src: 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YnJlYWtmYXN0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
     altText: 'Slide 3',
     caption: 'Slide 3',
     key: 3,
   },
 ];
 
-const RecipeHome = (items) =>{
+const RecipeHome = () =>{
+    console.log(items)
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -50,6 +51,7 @@ const RecipeHome = (items) =>{
   };
 
   const slides = items.map((item) => {
+   
     return (
       <CarouselItem
         onExiting={() => setAnimating(true)}
