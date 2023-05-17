a41b63df4839e6d726bf506b8903ded3d2315a7c
sam-saenz <samsaenz31@gmail.com>
2023-05-17 11:01:39 -0700

    Added currentUser to protected my recipes

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = ()=> {
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<LogIn />} />
         {/* <Route path='/recipeedit' element={<RecipeEdit />} /> */}
-        <Route path='/protectedmyrecipes' element={<ProtectedMyRecipes recipes={recipes}/>} />
+        <Route path='/protectedmyrecipes' element={<ProtectedMyRecipes recipes={recipes} currentUser={currentUser}/>} />
         <Route path='/notfound' element={<NotFound />} />
         <Route path='/navigation' element={<Navigation />} />
         <Route path='/header' element={<Header />} />
