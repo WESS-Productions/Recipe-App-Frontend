5c6eec2030678306c2effda99716f7c9a5d7c214
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-18 16:35:22 -0700

    styles to the header

--- src/componets/Navigation.js
+++ src/componets/Navigation.js
@@ -2,90 +2,95 @@ import React from "react"
 import { Nav, NavItem } from "reactstrap"
 import { NavLink, useNavigate } from "react-router-dom"
 import { useState } from "react"
-
-
+import YUM from "../assests/YUM.png"
 
 const Navigation = ({ current_user }) => {
-    const [currentUser, setCurrentUser] = useState(null)
-    const url = "http://localhost:3001"
+  const [currentUser, setCurrentUser] = useState(null)
+  const url = "http://localhost:3001"
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const logout = () => {
-        fetch(`${url}/logout`, {
-          headers: {
-            "Content-Type": 'application/json',
-            "Authorization": localStorage.getItem("token") //retrieve the token 
-          },
-          method: 'DELETE'
-        })
-        .then(payload => {
-        localStorage.removeItem("token")  // remove the token
+  const logout = () => {
+    fetch(`${url}/logout`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"), //retrieve the token
+      },
+      method: "DELETE",
+    })
+      .then((payload) => {
+        localStorage.removeItem("token") // remove the token
         setCurrentUser(null)
       })
-      .catch(error => console.log("log out errors: ", error))
-      }
-    
+      .catch((error) => console.log("log out errors: ", error))
+  }
 
-    const handleClick = () => {
-        logout()
-        navigate('/')
-      }
-      return(
-        <>
-            <Nav className="nav">
-                {current_user && (
-                    <>
-
-                        <NavLink to = '/'> 
-               {/* <img our logo asset will go here */}
-                        </NavLink>
-                        <NavItem>
-                            <NavLink to="/protectedmyrecipes" className="nav-link">
-                                My Recipes
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink to="/recipeindex" className="nav-link">
-                                Current Recipes
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink to="/newrecipe" className="nav-link">
-                                Add A New Recipe
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink onClick={handleClick} to="/" className="nav-link">
-                                Log Out
-                            </NavLink>
-                        </NavItem>
-                    </>
-                )}
-                {!current_user && (
-                    <>
-                        <NavLink to = '/'> 
-                            {/* our logo asset will go here */}
-                        </NavLink>
-                        <NavItem>
-                            <NavLink to="/recipeindex" className="nav-link">
-                                See Current Recipes
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink to="/signup" className="nav-link">
-                                Sign Up!
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink  to="/aboutus" className="nav-link">
-                                About us
-                            </NavLink>
-                        </NavItem>
-                    </>
-                )} 
-            </Nav>
-        </>
-    )
+  const handleClick = () => {
+    logout()
+    navigate("/")
+  }
+  return (
+    <>
+      <Nav className="justify-content-end" >
+        {current_user && (
+          <>
+            <NavLink to="/">
+              <img src={YUM} alt="Yum Logo" />
+            </NavLink>
+            <NavItem className="nav-link">
+              <NavLink to="/protectedmyrecipes" className="nav-link-styles">
+                My Recipes
+              </NavLink>
+            </NavItem>
+            <NavItem className="nav-link">
+              <NavLink to="/recipeindex" className="nav-link-styles">
+                Current Recipes
+              </NavLink>
+            </NavItem>
+            <NavItem className="nav-link">
+              <NavLink to="/newrecipe" className="nav-link-styles">
+                Add A New Recipe
+              </NavLink>
+            </NavItem>
+            <NavItem className="nav-link">
+              <NavLink onClick={handleClick} to="/" className="nav-link-styles">
+                Log Out
+              </NavLink>
+            </NavItem>
+          </>
+        )}
+        {!current_user && (
+          <>
+            <NavLink to="/">
+              <img src={YUM} alt="Yum Logo" className="nav-bar-logo" />
+            </NavLink>
+            
+              <NavItem className="nav-link">
+                <NavLink to="/recipeindex" className="nav-link-styles">
+                  Current Recipes
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-link">
+                <NavLink to="/signup" className="nav-link-styles">
+                  Sign Up!
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-link">
+                <NavLink to="/aboutus" className="nav-link-styles">
+                  About us
+                </NavLink>
+              </NavItem>
+        
+          </>
+        )}
+      </Nav>
+    </>
+  )
 }
 export default Navigation
+
+
+
+{/* <NavbarBrand tag={Link} to="/">
+        <img src={catLogo} alt="Cat outline" className="header-cat-logo" />
+      </NavbarBrand> */}
