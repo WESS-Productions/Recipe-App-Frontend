9f20c8c7bc3b927fe0747cf7148c5d909193f61e
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-19 09:30:13 -0700

    Nav bar finally complete..... for NOW muah hahahaha

--- src/componets/Navigation.js
+++ src/componets/Navigation.js
@@ -1,14 +1,26 @@
 import React from "react"
-import { Nav, NavItem } from "reactstrap"
-import { NavLink, useNavigate } from "react-router-dom"
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+} from "reactstrap"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 import { useState } from "react"
 import YUM from "../assests/YUM.png"
 
 const Navigation = ({ current_user }) => {
   const [currentUser, setCurrentUser] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
   const url = "http://localhost:3001"
 
   const navigate = useNavigate()
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen)
+  }
 
   const logout = () => {
     fetch(`${url}/logout`, {
@@ -19,7 +31,7 @@ const Navigation = ({ current_user }) => {
       method: "DELETE",
     })
       .then((payload) => {
-        localStorage.removeItem("token") 
+        localStorage.removeItem("token")
         setCurrentUser(null)
       })
       .catch((error) => console.log("log out errors: ", error))
@@ -30,64 +42,60 @@ const Navigation = ({ current_user }) => {
     navigate("/")
   }
   return (
-    <>
-      <Nav className="justify-content-end" >
-        {current_user && (
-          <>
-            <NavLink to="/">
-              <img src={YUM} alt="Yum Logo" />
-            </NavLink>
-            <NavItem className="nav-link">
-              <NavLink to="/protectedmyrecipes" className="nav-link-styles">
-                My Recipes
-              </NavLink>
-            </NavItem>
-            <NavItem className="nav-link">
-              <NavLink to="/recipeindex" className="nav-link-styles">
-                Current Recipes
-              </NavLink>
-            </NavItem>
-            <NavItem className="nav-link">
-              <NavLink to="/newrecipe" className="nav-link-styles">
-                Add A New Recipe
-              </NavLink>
-            </NavItem>
-            <NavItem className="nav-link">
-              <NavLink onClick={handleClick} to="/" className="nav-link-styles">
-                Log Out
-              </NavLink>
-            </NavItem>
-          </>
-        )}
-        {!current_user && (
-          <>
-            <NavLink to="/">
-              <img src={YUM} alt="Yum Logo" className="nav-bar-logo" />
-            </NavLink>
-            
-              <NavItem className="nav-link">
+    <Navbar light expand="md">
+      <NavbarBrand tag={Link} to="/" className="mr-auto">
+        <img src={YUM} alt="Yum Logo" className="nav-bar-logo" />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggleNav} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          {current_user && (
+            <>
+              <NavItem className="nav-item-spacing">
+                <NavLink to="/protectedmyrecipes" className="nav-link-styles">
+                  My Recipes
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item-spacing">
                 <NavLink to="/recipeindex" className="nav-link-styles">
                   Current Recipes
                 </NavLink>
               </NavItem>
-              <NavItem className="nav-link">
-                <NavLink to="/signup" className="nav-link-styles">
-                  Sign Up!
+              <NavItem className="nav-item-spacing">
+                <NavLink to="/newrecipe" className="nav-link-styles">
+                  Add A New Recipe
                 </NavLink>
               </NavItem>
-              <NavItem className="nav-link">
+              <NavItem className="nav-item-spacing">
+                <NavLink onClick={handleClick} to="/" className="nav-link">
+                  Log Out
+                </NavLink>
+              </NavItem>
+            </>
+          )}
+          {!current_user && (
+            <>
+              <NavItem className="nav-item-spacing">
+                <NavLink to="/recipeindex" className="nav-link-styles">
+                  Current Recipes
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item-spacing">
+                <NavLink to="/signup" className="nav-link-styles">
+                  Sign Up
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-item-spacing">
                 <NavLink to="/aboutus" className="nav-link-styles">
                   About us
                 </NavLink>
               </NavItem>
-        
-          </>
-        )}
-      </Nav>
-    </>
+            </>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
   )
 }
+
 export default Navigation
-
-
-
