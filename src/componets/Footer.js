dd88f3b57f9121a0304bc0acef2c6bbc238e10ea
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-19 16:57:38 -0700

    Footer styled

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,10 +1,28 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import company from "../assests/company.png"
+
 
 
 const Footer = () => {
     return (
         <>
-        <footer className="footer-styles">©WESS Productions</footer>
+       
+        <footer className="footer-styles">©
+            <a href="https://github.com/WESS-Productions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="intext-link">
+            <img src={company} className="footer-logo"/>
+            </a>
+            <NavLink to={'/aboutus'} className="footer-link-styling"> About us </NavLink>
+            {/* <br></br> */}
+            <NavLink to={'/signup'} className="footer-link-styling"> Sign up </NavLink>
+            {/* <br></br> */}
+            <NavLink to={'/login'} className="footer-link-styling"> Login </NavLink>
+            <NavLink to={'/'} className="footer-link-styling"> Home </NavLink>
+        </footer>
+     
         </>
     )
 
