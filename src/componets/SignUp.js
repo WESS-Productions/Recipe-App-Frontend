6848ef01051782b9eb799c9c1194c8c21f08a47d
wagnergon <wagnergon52@gmail.com>
2023-05-19 13:20:04 -0700

    signup form added

--- src/componets/SignUp.js
+++ src/componets/SignUp.js
@@ -1,13 +1,35 @@
-import React from "react"
-
+import React from "react";
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 const SignUp = () => {
-    return (
-        <>
-        <h2>Sign Up Here</h2>
-        </>
-    )
-
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col sm="6">
+          <Form>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            <FormGroup>
+              <Label for="fullName">Full Name</Label>
+              <Input type="text" id="fullName" placeholder="Enter your full name" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" id="email" placeholder="Enter your email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" id="password" placeholder="Enter your password" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="confirmPassword">Confirm Password</Label>
+              <Input type="password" id="confirmPassword" placeholder="Confirm your password" />
+            </FormGroup>
+            <Button color="primary" block>Sign Up</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default SignUp
+export default SignUp;
