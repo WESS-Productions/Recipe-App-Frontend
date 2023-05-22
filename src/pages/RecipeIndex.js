3a44a62e14b29b2e737bd47478eb728345158648
sam-saenz <samsaenz31@gmail.com>
2023-05-22 14:24:26 -0700

    Added an ingredient search bar and vegetarian checkbox

--- src/pages/RecipeIndex.js
+++ src/pages/RecipeIndex.js
@@ -1,29 +1,51 @@
-import React from "react"
+import React, { useState, Checkbox } from "react"
 import { NavLink } from "react-router-dom"
 import { Card } from "reactstrap"
 
 const RecipeIndex = ({ recipes }) => {
-  console.log(recipes)
-  return (
-    <>
-    <h1 className="header-index"><span className="Yum-index">Yum</span>my ideas</h1>
-    <div className="recipe-list">
-      {recipes.map((recipe, index) => (
+    console.log(recipes)
+
+    const [searchTerm, setSearchTerm] = useState("")
+    const [searchVegetarian, setSearchVegetarian] = useState(false)
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
+    const handleVeggieChange = () => {
+        setSearchVegetarian(!searchVegetarian)
+    }
+
+    return (
         <>
-        <div className="recipe-card" key={index}>
-        <h2 className="recipe-title">{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} className="card-image" />
-          <div className="link-button-container">
-          <NavLink to={`/recipeshow/${recipe.id}`}>
-            <button className="button-login" type="button" style={{height:"2rem"}}>View Recipe</button>
-          </NavLink>
-          </div>
-          </div>
+            <h1 className="header-index"><span className="Yum-index">Yum</span>my ideas</h1>
+
+            <div>
+                <input placeholder="Enter An Ingredient" onChange={handleChange} />
+            </div>
+
+            <div>
+                <input type="checkbox" id="checkbox" onChange={handleVeggieChange}/>
+                <label htmlFor="checkbox">Show only vegetarian recipes</label>
+            </div>
+
+            <div className="recipe-list">
+                {recipes.filter(recipe => recipe.vegetarian === true || recipe.vegetarian === searchVegetarian).filter(recipe => recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())).map((recipe, index) => (
+                    <>
+                        <div className="recipe-card" key={index}>
+                            <h2 className="recipe-title">{recipe.title}</h2>
+                            <img src={recipe.image} alt={recipe.title} className="card-image" />
+                            <div className="link-button-container">
+                                <NavLink to={`/recipeshow/${recipe.id}`}>
+                                    <button className="button-login" type="button" style={{ height: "2rem" }}>View Recipe</button>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </>
+                ))}
+            </div>
         </>
-      ))}
-    </div>
-    </>
-  )
+    )
 }
 
 export default RecipeIndex
