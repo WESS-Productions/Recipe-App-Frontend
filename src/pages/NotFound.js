f95c8035a2c9cba465e260390e84ed8b5b4f8ac9
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-23 11:09:07 -0700

    not found page done

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,20 @@
 import React from "react"
-
+import instructorimage from "../assests/instructorimage.png"
 
 const NotFound = () => {
-    return(
-        <>
-        <h1>Sorry no Recipes</h1>
-        </>
-    )
-
+  return (
+    <>
+      <div className="container">
+        <div className="picture-container">
+          <img src={instructorimage}></img>
+          
+        </div>
+      </div>
+      <h1 className="notfound-header">ERROR 404</h1>
+      
+     
+    </>
+  )
 }
 
 export default NotFound
